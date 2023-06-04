1d292b3935a4e164d41e4cc7910d8378441205c3
WPF196 <1962978635@qq.com>
2023-06-04 22:00:51 +0800

    add Timestamp and Inetaddress

---
+++ Timestamp.h
@@ -0,0 +1,22 @@
+#ifndef TIMESTAMP_H
+#define TIMESTAMP_H
+
+#include <iostream>
+#include <string>
+
+class Timestamp
+{
+public:
+    Timestamp();
+    // 防止隐式类型转换
+    explicit Timestamp(int64_t microSecondsSinceEpoch); 
+    static Timestamp now();
+    std::string toString() const;
+
+private:
+    int64_t microSecondsSinceEpoch_;
+
+};
+
+
+#endif
