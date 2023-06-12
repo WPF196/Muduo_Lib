93515f56b38efc8e7b962787983c292ad857ba61
WPF196 <1962978635@qq.com>
2023-06-12 20:59:10 +0800

    add EventLoopThread

---
+++ SRC/EventLoopThread.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "noncopyable.h"
+#include "Thread.h"
+
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <string>
+
+class EventLoop;
+
+class EventLoopThread : noncopyable
+{
+public:
+    using ThreadInitCallback = std::function<void(EventLoop*)>; 
+
+    EventLoopThread(const ThreadInitCallback &cb = ThreadInitCallback(), 
+        const std::string &name = std::string());
+    ~EventLoopThread();
+
+    EventLoop* startLoop();
+private:
+    void threadFunc();
+
+    EventLoop *loop_;       
+    bool exiting_;          // 退出标志
+    Thread thread_;         
+    std::mutex mutex_;      
+    std::condition_variable cond_;  // 条件变量
+    ThreadInitCallback callback_;   // 线程初始化回调
+};
